dba9a4335499ca10839ef382619c5f4a7645b61a
george-prado <gmeloprado@gmail.com>
2024-01-30 01:48:57 -0300

    feat: ViewModels implementation

---
+++ ViewModels/EditorCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+	public class EditorCategoryViewModel
+	{
+        [Required(ErrorMessage = "O campo 'nome' é obrigatório.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Por favor entre um nome válido.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo 'slug' é obrigatório.")]
+		[StringLength(30, MinimumLength = 3, ErrorMessage = "Por favor entre um slug válido.")]
+		public string Slug { get; set; }    
+    }
+}
---
+++ ViewModels/ResultViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Blog.ViewModels
+{
+	public class ResultViewModel<T>
+	{
+        //Retorna a saída
+        public ResultViewModel(T data, List<string> errors)
+        {
+            Data = data;
+            Errors = errors;
+        }
+
+        //Recebe acertos
+        public ResultViewModel(T data)
+        {
+            Data = data;
+        }
+
+        //Recebe erros bad request (string list)
+        public ResultViewModel(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        //Recebe erros not found (string)
+        public ResultViewModel(string error)
+        {
+            Errors.Add(error);
+        }
+
+        public T Data { get; private set; }
+        
+        public List<string> Errors { get; private set; } = new();
+    }
+}
