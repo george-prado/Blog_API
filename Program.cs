88fb85d77e752ce463561acd903b873880bc085a
george-prado <gmeloprado@gmail.com>
2024-01-30 01:46:51 -0300

    fix: suppressed ModelState auto-verify

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@ using Blog.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+{
+	options.SuppressModelStateInvalidFilter = true;
+});
 builder.Services.AddDbContext<BlogDataContext>();
 
 var app = builder.Build();
